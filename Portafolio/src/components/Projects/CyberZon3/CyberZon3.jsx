2cb0a61cf42d5c657ba9884e7aa8550f486c7303
aldofc <aldofc18@gmail.com>
2023-06-05 23:19:08 -0600

    part3-resposive

--- Portafolio/src/components/Projects/CyberZon3/CyberZon3.jsx
+++ Portafolio/src/components/Projects/CyberZon3/CyberZon3.jsx
@@ -13,9 +13,11 @@ const CyberZon3 = () => {
 
 
       <div className={style.container__rigth}>
-        <h1>CyberZon3</h1>
-        <p className={style.parrafo}>parrafo sobre suberzonellllllllllllllllllllllllllllll
-          llllllllllllllajbdjkahdajdhakdhajkdh
+        <h1 className={style.tittle}>CyberZon3</h1>
+        <p className={style.parrafo}>
+          Cyberzon3 es un E-commerce 100% funcional sobre ventas de computadoras con 
+          filtros, ordenamientos, detalles, lista de favoritos, pasarela de pagos, autenticacion,
+          dashboard de administrador etc.
           
         </p>
         <button className={style.btn}>Ver Proyecto</button>
