0360588cbb3f04c758fb994a8460c4bf8fe9d8e3
Aldo flores carreon <aldofc18@gmail.com>
2023-10-28 14:53:04 -0600

    general-updates-and-change-in-the-navnar

--- Portafolio/src/Pages/Home/Home.jsx
+++ Portafolio/src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import NabvarBoot from '../../components/NavbarBootStrap/NabvarBoot'
 import Part1 from '../../components/Parts/Part1/Part1'
 import Part2 from '../../components/Parts/Part2/Part2'
 import Part3 from '../../components/Parts/Part3/Part3'
@@ -12,7 +13,7 @@ const Home = () => {
         <div className={style.container__page__home}>
             <div>
                 <div className={style.container__top__navbar}>
-                    <Navbar />
+                    <NabvarBoot />
                 </div>
                 <Part1 />
                 <Part2 />
---
+++ Portafolio/src/components/NavbarBootStrap/NabvarBoot.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import style from './NavbarBoot.module.css'
+import Aldo from '../../assets/1D45E652-6516-49C4-AE67-3F02BF00C2DF.jpeg'
+
+const NabvarBoot = () => {
+  return (
+    <div className={style.container__fixed}>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+   <img className={style.size__img} src={Aldo} alt="" />
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <div>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#home"><p className={style.myname__style}>Home</p></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#aboutme"><p className={style.myname__style}>Acerca de mi</p></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#projects"><p className={style.myname__style}>Proyectos</p></a>
+        </li>
+        {/* <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown link
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li> */}
+      </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+    </div>
+  )
+}
+
+export default NabvarBoot
